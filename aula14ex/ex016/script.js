bf9e8e7d7333d72d0054de34514e498762728a9d
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-05-27 14:12:07 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,15 +4,29 @@ function contar() {
     let passo = document.querySelector('input#txtp')
     let res = document.querySelector('div#res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        // window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // Contagem decrescente
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }   
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
         
 }
