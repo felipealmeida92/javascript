6fde9f697d9a8fe7873361fe1ccfd0cf046c2b36
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-05-27 11:47:26 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,7 +3,7 @@ function contador() {
     var fi = document.querySelector('input#fim')
     var pas = document.querySelector('input#passo')
     var res = document.querySelector('div#res')
-    for (Number(ini.value); Number(fi.value); Number(pas.value)) {
+    for (Number(ini.value); Number(fi.value); Number(pas.value)++) {
         res.innerHTML = `${Number(ini.value)}`
     }
 }
