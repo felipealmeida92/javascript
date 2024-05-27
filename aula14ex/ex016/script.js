d3b7351eae5d2364a14795b62cba9d31e7e2bb5b
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-05-27 12:10:22 -0300

    Update ex16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,9 +1,18 @@
-function contador() {
-    var ini = document.querySelector('input#inicio')
-    var fi = document.querySelector('input#fim')
-    var pas = document.querySelector('input#passo')
-    var res = document.querySelector('div#res')
-    for (Number(ini.value); Number(fi.value); Number(pas.value)++) {
-        res.innerHTML = `${Number(ini.value)}`
+function contar() {
+    let ini = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    let res = document.querySelector('div#res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} `
+        }
     }
+        
 }
