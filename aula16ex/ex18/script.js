dba7602862f5e307f4169710fad2ae51cff58986
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-05-27 19:28:57 -0300

    Update script.js

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -1,7 +1,6 @@
 
-let num = document.querySelector('input#txtn')
-let res = document.querySelector('select#exsel')
-let fim = document.getElementById('resultado')
+let num = document.querySelector('input#fnum')
+let res = document.querySelector('select#flista')
 function analisar(n=0) {
     if (num.value.length == 0 || num.value > 100) {
         alert('Valor inválido ou já encontrado na lista.')
@@ -14,5 +13,5 @@ function analisar(n=0) {
 }
 
 function finalisar() {
-    fim.innerHTML = `Adicionado`
+    
 }
